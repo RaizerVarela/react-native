a0b6074bf0e9e3d1bbf2d575bc7edce73bff5d33
Raizer Varela <raizer.varela@gmail.com>
2022-03-14 10:16:04 -0300

    Adição do MinMax.js - passagem de parametro entre componentes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
-import CompPadrao, {Comp1, Comp2} from './components/Multi'
-import Primeiro from './components/Primeiro';
+import MinMax from './components/MinMax';
+//import CompPadrao, {Comp1, Comp2} from './components/Multi'
+//;import Primeiro from './components/Primeiro';
 
 export default () => (
   <View style={style.App}>
-    <CompPadrao/>
+    <MinMax min={3} max={20}/>
+    <MinMax min={1} max={94}/>
+    {/*<CompPadrao/>
     <Comp1/>
     <Comp2/>
-    <Primeiro />
-  </View>
+    <Primeiro />*/}
+</View>
 ); // Código em JSX
 
 
@@ -18,6 +21,7 @@ const style = StyleSheet.create({
   App: {
     flexGrow: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    padding: 20
   }
 })
