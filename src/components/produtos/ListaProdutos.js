d423c50d451e51a64e22c89c852811c11cae72e4
Raizer Varela <raizer.varela@gmail.com>
2022-03-23 14:14:23 -0300

    Adição - Renderização de lista e utilização do atributo key

---
+++ src/components/produtos/ListaProdutos.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Text} from 'react-native'
+import Estilo from '../estilo'
+import produtos from './Produtos'
+
+import Produtos from './Produtos'
+
+export default props=>{
+  
+  function obterLista(){
+    return produtos.map(p=>{
+      return (
+      <Text key={p.id}>
+        {p.id}) {p.nome} tem preço R$ {p.preco}
+      </Text>
+      )
+    })
+  }
+
+  return (
+    <>
+      <Text style={Estilo.fontG}>
+        Lista de produtos   
+      </Text>
+      {obterLista()}
+    </>
+  )
+}
