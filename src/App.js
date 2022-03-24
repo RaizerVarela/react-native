ac0bdadf20bde58e924c004d77c267db77c75394
Raizer Varela <raizer.varela@gmail.com>
2022-03-24 20:41:02 -0300

    Atualização - novos conteudos para serem exibidos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 
-import ContadorV2 from './components/contador/ContadorV2';
+import Mega from './components/mega/Mega';
+//import FlexBoxV4 from './components/layout/FlexBoxV4';
+//import FlexBoxV3 from './components/layout/FlexBoxV3';
+//import FlexBoxV2 from './components/layout/FlexBoxV2';
+//import FlexBoxV1 from './components/layout/FlexBoxV1';
+//import DigiteSeuNome from './components/DigiteSeuNome';
+//import ListaProdutosV2 from './components/produtos/ListaProdutosV2';
+//import ListaProdutos from './components/produtos/ListaProdutos';
+//import UsuarioLogado from './components/UsuarioLogado';
+//import Familia from './components/relacao/Familia';
+//import Membro from './components/relacao/Membro';
+//import ParImpar from './components/ParImpar';
+//import Diferenciar from './components/Diferenciar';
+//import ContadorV2 from './components/contador/ContadorV2';
 //import Pai from './components/indireta/Pai'
 //import Pai from './components/direta/Pai'
 //import Contador from './components/Contador'
@@ -13,9 +26,28 @@ import ContadorV2 from './components/contador/ContadorV2';
 //import Primeiro from './components/Primeiro';
 
 export default () => (
-  <View style={style.App}>
+  <SafeAreaView style={style.App}>
+  <Mega/>
+  {/*
+  <FlexBoxV4/>
+  <FlexBoxV3/>
+  <FlexBoxV2/>
+  <FlexBoxV1/>
+  <DigiteSeuNome/>
+  <ListaProdutosV2/>
+  <ListaProdutos/>
+  <UsuarioLogado usuario={{nome: 'Rai', email: 'rai@rai.com'}}/>
+  <Familia>
+    <Membro nome="Bia" sobrenome="Arruda"/>
+    <Membro nome="Carlos" sobrenome="Arruda"/>
+  </Familia>
+  <Familia>
+    <Membro nome="Ana" sobrenome="Silva"/>
+    <Membro nome="Julia" sobrenome="Silva"/>
+  </Familia>
+    <ParImpar num={4}/>
+    <Diferenciar/>
     <ContadorV2/>
-    {/*
     <Pai/>
     <Pai/>
     <Contador inicial={100} passo={13}/>
@@ -29,7 +61,7 @@ export default () => (
     <Comp1/>
     <Comp2/>
     <Primeiro />*/}
-</View>
+</SafeAreaView>
 ); // Código em JSX
 
 
