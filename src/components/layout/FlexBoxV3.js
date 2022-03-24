849797900fabba754ea1697682d6e018b7454143
Raizer Varela <raizer.varela@gmail.com>
2022-03-24 20:12:46 -0300

    Adição - FlexBox alteração do eixo principal com o uso do flexDirection

---
+++ src/components/layout/FlexBoxV3.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {StyleSheet, View} from 'react-native'
+
+import Quadrado from './Quadrado'
+
+export default props=>{
+  return (
+    <View style={style.FlexV2}>
+      <Quadrado cor='#009' lado={20}/>
+      <Quadrado cor='#F00' lado={30}/>
+      <Quadrado cor='#0F0' lado={40}/>
+      <Quadrado cor='#009' lado={50}/>
+      <Quadrado cor='#0F0' lado={60}/>
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+  FlexV2: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'baseline',
+    height: 350,
+    width:'100%',
+    backgroundColor:'#000'
+  }
+})
