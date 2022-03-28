065b59d4f0892bb158e1e36bfd9d3a6a3f73a119
Raizer Varela <raizer.varela@gmail.com>
2022-03-28 11:07:47 -0300

    Ajustes

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -1,13 +1,30 @@
 import React from 'react'
-import {Text} from 'react-native'
+import {Text, TextInput} from 'react-native'
 import Estilo from '../estilo'
 
-export default class Mega extends React.Component{
+export default class Mega extends React.Component{  
+  state={
+    qtdeNumeros: this.props.qtdeNumeros
+  }
+
+  alterarQtdeNumero=(qtde)=>{
+    this.setState({qtdeNumeros: qtde})
+  }
+
   render(){
     return(
-      <Text style={Estilo.fontG}>
-        Gerador de Mega-Sena {this.props.qtdeNumeros}
-      </Text>
+      <>
+        <Text style={Estilo.fontG}>
+          Gerador de Mega-Sena {this.state.qtdeNumeros}
+        </Text>
+        <TextInput
+          keyboardType={'numeric'}
+          style={{borderBottomWidth: 1}}
+          placeholder="Quantidade de números"
+          value={this.state.qtdeNumeros}
+          onChangeText={this.alterarQtdeNumero}
+        />
+      </>
     )
   }
 }
